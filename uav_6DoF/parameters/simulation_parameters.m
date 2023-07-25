e562094666fd3e958122679e42a7b337ea26eda6
sohail-nadaf <sohailnadaf.edu@gmail.com>
2023-07-25 11:59:36 +0530

    added parameters for simulation

---
+++ uav_6DoF/parameters/simulation_parameters.m
@@ -0,0 +1,10 @@
+% simulation parameters
+% - to hold sim params
+
+SIM.ts_simulation = 0.02;  % smallest time step for simulation
+SIM.ts_control = SIM.ts_simulation;
+SIM.ts_plotting = 0.5;
+SIM.ts_video = 0.1;
+
+SIM.start_time = 0;
+SIM.end_time = 30;
